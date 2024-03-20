33396f202f713e7ecd13b18e4700a35b3dadd002
Manav3008 <122639562+Manav3008@users.noreply.github.com>
2024-03-19 22:59:38 -0400

    Make some changes in Home Page

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './HomePage.css'; // Make sure to create a corresponding CSS file
+import developerWorkspace from './images/com.png';
 
 const HomePage = () => {
   return (
@@ -8,12 +9,19 @@ const HomePage = () => {
         {/* Add your navigation items here */}
         <a href="#contact" className="contact-button">Contact</a>
       </nav>
+      <div className="content">
+
       <section className="hero">
         <h1>Front-End Software Developer</h1>
         <p>Resolving design problems, building smart user interfaces and useful interactions, developing rich web applications and seamless web experiences.</p>
-        <a href="#about" className="about-button">About me</a>
+        <a href="#about" className="about-button">About me </a>
+        </section>
+
+        <div class="image-container">
+        <img src={developerWorkspace} alt="Developer Workspace" />
+        </div>
+        </div>
         {/* Add more content and sections as needed */}
-      </section>
       <footer className="footer">
         <span>Scroll Down</span>
         {/* Include any additional information or icons here */}
